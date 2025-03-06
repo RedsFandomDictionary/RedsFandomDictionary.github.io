7bab5ad877cff0b1eff5dc33e406db5bbb8903d8
RedsFandomDictionary <158115079+RedsFandomDictionary@users.noreply.github.com>
2025-03-05 21:14:10 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,8 +1,24 @@
-let selectedTropes = document.querySelectorAll('input[type="checkbox"]:checked');
+const selectedTropes = document.querySelectorAll('input[type="checkbox"]:checked');
+const resultsText = document.getElementById("results-text");
+const resultsBtn = document.getElementById("results-btn");
 
+resultsBtn.addEventListener("click", updateScoreText(getScore()));
 
-function updateScoreText(){
-
+function updateScoreText(score){
+resultsText.textContent = `You've scored ${score} out of !`
+  
+  if (score > 5){
+  } else if (score > 4){
+    
+  } else if (score > 3){
+    
+  } else if (score > 2){
+    
+  } else{
+    
+  }
+      
+    
 }
 
 
