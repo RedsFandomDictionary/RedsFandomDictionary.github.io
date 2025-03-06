743d5de9cd0991ed3f9cfa6dfe22a7105f834bcd
RedsFandomDictionary <158115079+RedsFandomDictionary@users.noreply.github.com>
2025-03-05 21:02:14 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,15 @@
+let selectedTropes = document.querySelectorAll('input[type="checkbox"]:checked');
 
+
+function updateScoreText(){
+
+}
+
+
+function getScore(){
+  let score = 0;
+  for (i=0; i < selectedTropes.length; i++){
+  score += selectedTropes[i].value; 
+  }
+  return score;
+}
